e474c7e379e205bb94ca187cf2695e2262f01821
Marc <rodo.leon.marc@gmail.com>
2023-05-02 16:37:44 +0200

    Mes funcionalitats

--- src/main/java/dam2/recuperaciom06uf2/ControladorLlibres.java
+++ src/main/java/dam2/recuperaciom06uf2/ControladorLlibres.java
@@ -6,7 +6,26 @@ import javafx.fxml.FXML;
 public class ControladorLlibres {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private void tornarEnrere() {
+        try {
+            App.setRoot("Principal");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void afegir() throws IOException {
+        System.out.println("Aquest metode encara no fa res");
+    }
+
+    @FXML
+    private void modificar() throws IOException {
+        System.out.println("Aquest metode encara no fa res");
+    }
+
+    @FXML
+    private void eliminar() throws IOException {
+        System.out.println("Aquest metode encara no fa res");
     }
 }
--- src/main/java/dam2/recuperaciom06uf2/ControladorUsuaris.java
+++ src/main/java/dam2/recuperaciom06uf2/ControladorUsuaris.java
@@ -6,7 +6,26 @@ import javafx.fxml.FXML;
 public class ControladorUsuaris {
 
     @FXML
-    private void tornarEnrere() throws IOException {
-        App.setRoot("Principal");
+    private void tornarEnrere() {
+        try {
+            App.setRoot("Principal");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void afegir() throws IOException {
+        System.out.println("Aquest metode encara no fa res");
+    }
+
+    @FXML
+    private void modificar() throws IOException {
+        System.out.println("Aquest metode encara no fa res");
+    }
+
+    @FXML
+    private void eliminar() throws IOException {
+        System.out.println("Aquest metode encara no fa res");
     }
 }
