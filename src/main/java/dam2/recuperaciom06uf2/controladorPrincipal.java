064e3595908b718bea9a39065b79c3f4af03281c
Marc <rodo.leon.marc@gmail.com>
2023-05-02 16:16:30 +0200

    Començo amb el disseny de les pantalles i a fer el funcionament

---
+++ src/main/java/dam2/recuperaciom06uf2/controladorPrincipal.java
@@ -0,0 +1,22 @@
+package dam2.recuperaciom06uf2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class controladorPrincipal {
+
+    @FXML
+    private void usuaris() {
+        System.out.println("Aquest metode encara no fa res");
+    }
+
+    @FXML
+    private void prestec() {
+        System.out.println("Aquest metode encara no fa res");
+    }
+
+    @FXML
+    private void llibres() {
+        System.out.println("Aquest metode encara no fa res");
+    }
+}
