58e60bf42a4f0ab658f47b7ee4d5ade24a635ec1
= <=>
2023-05-03 13:28:13 +0200

    Em dona un error

---
+++ src/main/java/Classes/SingleSession.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author marcr
+ */
+public class SingleSession {
+       private static SingleSession instancia;
+        private SessionFactory factory;
+        private Session s;
+        
+        private SingleSession() {
+            InitSessionFactory();
+        }
+        
+        public static SingleSession getInstance()
+        {
+            if (instancia == null)
+                instancia = new SingleSession();
+            
+            return instancia;
+        }
+        
+        /***
+         * Punt on es configura hibernate
+         */
+        private void InitSessionFactory() {
+                Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+                // Just en aquest punt, podem modificar els valors per defecte i assignar-ne d'altres
+                // Aquesta és la llista de propietats disponibles: https://docs.jboss.org/hibernate/orm/4.3/javadocs/
+                configuration.setProperty("hibernate.connection.username", "Marc");
+                configuration.setProperty("hibernate.connection.password", "123");
+                configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/recuperacio?createDatabaseIfNotExist=true");
+                
+           try {
+               this.factory = configuration.buildSessionFactory();
+           } catch (HibernateException ex) {
+               ex.printStackTrace();
+           }
+                
+                // en aquest punt una sessió
+                s = SessionFactory.openSession();
+	}
+
+        /***
+         * Retorna la mateixa sessió
+         * @return 
+         */
+        public Session getSessio() {
+               
+            return s;
+        }
+        
+        public void closeFactory()
+        {
+            factory.close();
+        }
+}
--- src/main/java/dam2/recuperaciom06uf2/App.java
+++ src/main/java/dam2/recuperaciom06uf2/App.java
@@ -1,12 +1,11 @@
 package dam2.recuperaciom06uf2;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
