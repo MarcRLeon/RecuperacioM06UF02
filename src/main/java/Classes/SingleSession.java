5f8be954ecf5d06d409bab91a315c4af2ee9676b
Marc <rodo.leon.marc@gmail.com>
2023-05-03 15:46:57 +0200

    de moment tots els problemes solucionats

--- src/main/java/Classes/SingleSession.java
+++ src/main/java/Classes/SingleSession.java
@@ -14,54 +14,58 @@ import org.hibernate.cfg.Configuration;
  * @author marcr
  */
 public class SingleSession {
-       private static SingleSession instancia;
-        private SessionFactory factory;
-        private Session s;
-        
-        private SingleSession() {
-            InitSessionFactory();
-        }
-        
-        public static SingleSession getInstance()
-        {
-            if (instancia == null)
-                instancia = new SingleSession();
-            
-            return instancia;
-        }
-        
-        /***
-         * Punt on es configura hibernate
-         */
-        private void InitSessionFactory() {
-                Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-                // Just en aquest punt, podem modificar els valors per defecte i assignar-ne d'altres
-                // Aquesta és la llista de propietats disponibles: https://docs.jboss.org/hibernate/orm/4.3/javadocs/
-                configuration.setProperty("hibernate.connection.username", "Marc");
-                configuration.setProperty("hibernate.connection.password", "123");
-                configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/recuperacio?createDatabaseIfNotExist=true");
-                
-           try {
-               this.factory = configuration.buildSessionFactory();
-           } catch (HibernateException ex) {
-               ex.printStackTrace();
-           }
-                
-                // en aquest punt una sessió
-                s = SessionFactory.openSession();
-	}
 
-        /***
-         * Retorna la mateixa sessió
-         * @return 
-         */
-        public Session getSessio() {
-               
-            return s;
+    private static SingleSession instancia;
+    private SessionFactory factory, f2;
+    private Session s;
+
+    private SingleSession() {
+        InitSessionFactory();
+    }
+
+    public static SingleSession getInstance() {
+        if (instancia == null) {
+            instancia = new SingleSession();
         }
-        
-        public void closeFactory()
-        {
-            factory.close();
+
+        return instancia;
+    }
+
+    /**
+     * *
+     * Punt on es configura hibernate
+     */
+    private void InitSessionFactory() {
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+        // Just en aquest punt, podem modificar els valors per defecte i assignar-ne d'altres
+        // Aquesta és la llista de propietats disponibles: https://docs.jboss.org/hibernate/orm/4.3/javadocs/
+        configuration.setProperty("hibernate.connection.username", "Marc");
+        configuration.setProperty("hibernate.connection.password", "123");
+        configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/recuperacio?createDatabaseIfNotExist=true");
+
+        try {
+            this.factory = configuration.buildSessionFactory();
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
         }
+
+        // en aquest punt una sessió
+        s = f2.openSession();
+
+    }
+
+    /**
+     * *
+     * Retorna la mateixa sessió
+     *
+     * @return
+     */
+    public Session getSessio() {
+
+        return s;
+    }
+
+    public void closeFactory() {
+        factory.close();
+    }
 }
