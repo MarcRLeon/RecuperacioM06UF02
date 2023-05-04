ff34e3a5f4c81be061b404e99a8e328c4fccf074
Marc <rodo.leon.marc@gmail.com>
2023-05-04 16:35:30 +0200

    ja es pot cambiar de pantalla be

--- src/main/java/dam2/recuperaciom06uf2/ControladorLlibres.java
+++ src/main/java/dam2/recuperaciom06uf2/ControladorLlibres.java
@@ -1,14 +1,29 @@
 package dam2.recuperaciom06uf2;
 
 import java.io.IOException;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class ControladorLlibres {
 
     @FXML
-    private void tornarEnrere() {
+    private void tornarEnrere(Event event) {
         try {
-            App.setRoot("Principal");
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Obtiene el Stage de la ventana actual
+            currentStage.hide(); // Oculta la ventana actual
+
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Principal.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 550, 350); // Establece las dimensiones deseadas
+            newStage.setScene(scene);
+            newStage.show();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
--- src/main/java/dam2/recuperaciom06uf2/controladorPrincipal.java
+++ src/main/java/dam2/recuperaciom06uf2/controladorPrincipal.java
@@ -1,33 +1,65 @@
 package dam2.recuperaciom06uf2;
 
 import java.io.IOException;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class controladorPrincipal {
 
     @FXML
-    private void usuaris() {
+    private void usuaris(Event event) {
         try {
-            App.setRoot("PantallaUsuaris");
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Obtiene el Stage de la ventana actual
+            currentStage.hide(); // Oculta la ventana actual
+
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("PantallaUsuaris.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 670, 500); // Establece las dimensiones deseadas
+            newStage.setScene(scene);
+            newStage.show();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     @FXML
-    private void prestec() {
+    private void prestec(Event event) {
         try {
-            App.setRoot("PantallaPrestec");
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Obtiene el Stage de la ventana actual
+            currentStage.hide(); // Oculta la ventana actual
+
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("PantallaPrestec.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 700, 500); // Establece las dimensiones deseadas
+            newStage.setScene(scene);
+            newStage.show();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
     }
 
     @FXML
-    private void llibres() {
+    private void llibres(Event event) {
         try {
-            App.setRoot("PantallaLlibres");
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Obtiene el Stage de la ventana actual
+            currentStage.hide(); // Oculta la ventana actual
+
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("PantallaLlibres.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 700, 500); // Establece las dimensiones deseadas
+            newStage.setScene(scene);
+            newStage.show();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
